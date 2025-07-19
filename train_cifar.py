7c4c460b28d7e24ab626bb5995a8c7878bb2f10b
Syed Hassan Ali <imsyedhassanali@gmail.com>
2025-07-19 16:31:40 +0500

    Update train_cifar.py

--- train_cifar.py
+++ train_cifar.py
@@ -67,7 +67,7 @@ val_loader = torch.utils.data.DataLoader(
 
 
 if __name__ == '__main__':
-    if datasets.lower() == "cifar100":
+    if datasets == "cifar100":
         num_classes = 100
         swin_type = 'tiny'
         reg_type, reg_lambda = 'l1', 1e-5
